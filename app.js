3001d8ad9bddac82271de6d36970626d608d0d9d
sara-shpigel <sara671876@gmail.com>
2024-04-09 14:38:23 +0300

    first commit

--- app.js
+++ app.js
@@ -1,46 +1,29 @@
 const express = require('express');
-const cors = require('cors');
-const mysql = require('mysql2');
+const bodyParser = require('body-parser');
 
 const app = express();
+const PORT = 3001;
 
-const corsOptions = {
-    origin: 'http://localhost:3000',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-    optionsSuccessStatus: 204,
-  };
-  
-  app.use(cors(corsOptions));
-  
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'shopping-list'
+const cors = require('cors');
+app.use(cors());
+
+let orders = [];
+let orderId = 0;
+
+app.use(bodyParser.json());
+
+app.post('/orders', (req, res) => {
+  const { order } = req.body
+  const orderObject = {  id: orderId++, name: order };
+  orders.push(orderObject);
+  res.status(201).json({ message: 'Order received successfully' });
 });
 
-db.connect((err) => {
-  if (err) {
-    throw err;
-  }
-  console.log('Sql connect');
+app.get('/orders', (req, res) => {
+  console.log(orders);
+  res.json(orders);
 });
 
-app.get('/categories', (req, res) => {
-  const sql = 'SELECT * FROM categories';
-  db.query(sql, (err, result) => {
-    if (err) throw err;
-    res.json(result);
-  });
-});
-
-const port = process.env.PORT || 3003;
-app.listen(port, () => {
-  console.log(`The server in port: ${port}`);
-});
-
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Error in the server!');
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
